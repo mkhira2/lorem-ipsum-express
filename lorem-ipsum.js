52bc2721dcc71245aafee578c42ec983da8ae695
Kenji Hirabayashi <mkhira2@gmail.com>
2017-06-18 19:03:43 -0500

    small node/express example for may_2017 cohort

---
+++ lorem-ipsum.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+var loremIpsum = require('lorem-ipsum')
+var output     = loremIpsum({count: 3, units: 'paragraphs', suffix: '<br />'})
+
+function addLorem() {
+  return output
+}
+
+app.get('/', function (req, res) {
+  res.send('Lorem Ipsum' + '<br /><br />' + 'Add /lorem to the url to get some lorem up in here!')
+})
+
+app.get('/lorem', function (req, res) {
+  res.send(addLorem())
+})
+
+app.get('/lorem/:tagID', function (req, res) {
+  res.send(loremIpsum({count: req.params.tagID, units: 'paragraphs', suffix: '<br />'}))
+})
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!')
+})
